1609e164df1527c199eb9eef560a0cc8b6aeaff8
pc <kunals.eleven@gmail.com>
2018-09-30 13:33:21 -0400

    Added Fileparser and GameConstants classes to parse the map file.

---
+++ Risk/src/domain/GameConstants.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class GameConstants {
+	public static String CONTINENT_KEY= "[Continents]";
+	public static String TERRITORY_KEY= "[Territories]";
+	public static String TERRITORY_SET_KEY= "Territories";
+	public static String CONTINENT_SET_KEY= "Continents";
+
+}
