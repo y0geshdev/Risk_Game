0f769c522dd830ec98c8c0a8d696c74f06b2c093
pc <kunals.eleven@gmail.com>
2018-10-14 22:29:06 -0400

    Adding test cases.

--- Risk/test/service/MapServiceTest.java
+++ Risk/test/service/MapServiceTest.java
@@ -1,10 +1,17 @@
 package service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import controller.GameController;
+import controller.MapController;
 
 /**
  * This Junit Test Class will have all the test cases for {@link MapService}
@@ -15,19 +22,63 @@ import controller.GameController;
  */
 public class MapServiceTest {
 
-	@Test
+	File nullFileObject, correctFileObject, wrongFileObject;
+
+	@Before
+	public void beforEachTestCase() {
+		nullFileObject = null;
+		correctFileObject = new File("C:\\Users\\pc\\Desktop\\apprisk\\Asiamap.map");
+		wrongFileObject = new File("C:\\Users\\pc\\Desktop\\apprisk\\WrongFormatMap.map");
+	}
+
+	@Ignore
 	public void testValidateMap() {
 		fail("Not yet implemented");
 	}
 
-	@Test
+	@Ignore
 	public void testSaveMap() {
 		fail("Not yet implemented");
 	}
 
-	@Test
+	/**
+	 * Tests parseFile() method for different type of map file Objects
+	 * @param errorMessage represents display message for different errors
+	 */ 
+ 
+@Test
 	public void testParseFile() {
-		fail("Not yet implemented");
+		MapService mp = new MapService();
+		try {
+			mp.parseFile(nullFileObject);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			String errorMessage = "File not Found";
+			assertEquals(errorMessage, e.getMessage());
+			e.printStackTrace();
+		}
+
+		try {
+			mp.parseFile(correctFileObject);
+			assertTrue(MapController.continentsSet.size() >= 1);
+			assertTrue(MapController.territoriesSet.size() >= 1);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			mp.parseFile(wrongFileObject);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			String errorMessageForZeroContinent = "No Continent is present in the File";
+			assertEquals(errorMessageForZeroContinent, e.getMessage());
+			String errorMessageForZeroTerritory = "No Territory is present in the File";
+			assertEquals(errorMessageForZeroTerritory, e.getMessage());
+
+			e.printStackTrace();
+		}
 	}
 
 }
