950b8435c3372d3cd825cf60e7abc4c7c62cfa48
pc <kunals.eleven@gmail.com>
2018-10-18 20:41:35 -0400

    Javadoc related changes

--- Risk/src/domain/Player.java
+++ Risk/src/domain/Player.java
@@ -13,36 +13,66 @@ public class Player {
 	private String name;
 	private List<Territory> territories;
 	private int armyCount;
-
+	
+	/**
+	 * default constructor for Player
+	 */
 	public Player() {
 		armyCount = 0;
 		territories = new ArrayList<>();
 	}
-
+	
+	/**
+     * Gets the name of this player
+     * 
+     * @return this player's name
+     */
 	public String getName() {
 		return name;
 	}
-
+	
+	/**
+     * Changes the name for this player
+     * 
+     * @param name:
+     * 			   This is the new name for this player
+     */
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
+	/**
+	 * gets the list of territories
+	 * @return the list of territories player owns
+	 */
 	public List<Territory> getTerritories() {
 		return territories;
 	}
-
+	
+	/**
+	 * Changes the territories associated with this player
+	 * @param territories 
+	 * 					This is the list of territories player owns
+	 */
 	public void setTerritories(List<Territory> territories) {
 		this.territories = territories;
 	}
-
+	/**
+	 * sets the army count for this player
+	 * @param armyCount:
+	 * 					Number of armies that this player owns
+	 */
 	public void setArmyCount(int armyCount) {
 		this.armyCount = armyCount;
 	}
-
+	/**
+	 * gets the number of armies that this player owns 
+	 * @return the number of armies that player owns
+	 */
 	public int getArmyCount() {
 		return armyCount;
 	}
-
+	
 	@Override
 	public String toString() {
 		return this.name;
--- Risk/src/domain/Territory.java
+++ Risk/src/domain/Territory.java
@@ -14,61 +14,114 @@ public class Territory {
     private Continent continent;
     private Player owner;
     private int armyCount;
-
+    /**
+     * default constructor for territory
+     */
     public Territory() {
      this.neighbourTerritories = new ArrayList<>();
      armyCount	=	0;
     }
-
+    /**
+     * Creates a territory with given name and associated continent
+     * @param name:
+     * 			Name for the new territory
+     * @param continent:
+     * 				object of the associated continent
+     */
     public Territory(String name, Continent continent) {
         this.name = name;
         this.continent = continent;
         this.neighbourTerritories = new ArrayList<>();
         armyCount	=	0;
     }
-
+    /**
+     * Creates a territory with given name
+     * @param name:
+     * 			Name for the new territory
+     */
     public Territory(String name){
         this.name = name;
         this.neighbourTerritories = new ArrayList<>();
         armyCount	=	0;
     }
-
+    /**
+     * Gets the name for the territory
+     * @return the name of this territory
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * Changes the name of this territory
+     * @param name:
+     * 			the new name for this territory
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    /**
+     * Gets the neighbouring territories for this territory
+     * @return the list of neighbouring territories for this territory
+     */
     public List<Territory> getNeighbourTerritories() {
         return neighbourTerritories;
     }
-
+    /**
+     * Sets the neighbouring territories for this territory
+     * @param neighbourTerritories:
+     * 							List of neighbouring territories for this territory
+     */
     public void setNeighbourTerritories(List<Territory> neighbourTerritories) {
         this.neighbourTerritories = neighbourTerritories;
     }
-
+    
+    /**
+     * Gets the continent object associated with this territory
+     * @return associated continent object
+     */
     public Continent getContinent() {
         return continent;
     }
-
+    
+    /**
+     * Changes the associated continent for this territory
+     * @param continent:
+     * 				Object of continent that contains this territory
+     */
     public void setContinent(Continent continent) {
         this.continent = continent;
     }
 
+    /**
+     * Gets the object of player who owns this territory
+     * @return object of this player
+     */
     public Player getOwner() {
         return owner;
     }
-
+    
+    /**
+     * Sets the player who owns this territory
+     *
+     * @param owner:
+     * 			Object of the player owning this territory
+     */
     public void setOwner(Player owner) {
         this.owner = owner;
     }
     
+    /**
+     * Gets number of armies that this territory has
+     * @return this number of armies
+     */
     public int getArmyCount() {
     	return armyCount;
     }
-    
+    /**
+     * Sets number of armies for this territory
+     * @param numberOfArmies:
+     * 				Number of armies that territory has
+     */
     public void setArmyCount(int numberOfArmies) {
     	this.armyCount	=	numberOfArmies;
     }
