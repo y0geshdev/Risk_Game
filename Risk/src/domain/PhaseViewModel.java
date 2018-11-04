2c307dfd5115b92f529fa2313cb0686a9e092c34
karanbhalla <kbhalla22@gmail.com>
2018-11-03 21:27:44 -0400

    Added javadoc for PhaseViewModel

--- Risk/src/domain/PhaseViewModel.java
+++ Risk/src/domain/PhaseViewModel.java
@@ -1,19 +1,34 @@
 package domain;
 
 import java.util.Observable;
-
+/**
+ * This class extends the Observable class and keeps a track of all the Observers.
+ * @author karanbhalla
+ *
+ */
 public class PhaseViewModel extends Observable {
 
 	private String currentPhase;
 	private String currentPlayer;
 	private String phaseInfo;
-	
+	/**
+	 * Gets the name of the current phase.
+	 * @return the currentPhase of the player.
+	 */
 	public String getCurrentPhase() {
 		return currentPhase;
 	}
+	/**
+	 * Gets the name of current player.
+	 * @return the current player's name.
+	 */
 	public String getCurrentPlayer() {
 		return currentPlayer;
 	}
+	/**
+	 * Gets the information about the current phase.
+	 * @return the phase info.
+	 */
 	public String getPhaseInfo() {
 		return phaseInfo;
 	}
