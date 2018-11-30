809e209a05712da9831734e16be0cefd4d16c14a
yogesh.kumar <m18nikhilkumar@gmail.com>
2018-11-30 12:44:50 +0530

    updating from remote

--- Risk/test/domain/DomainTestSuite.java
+++ Risk/test/domain/DomainTestSuite.java
@@ -10,7 +10,7 @@ import org.junit.runners.Suite.SuiteClasses;
  *
  */
 @RunWith(Suite.class)
-@SuiteClasses({ PlayerTest.class })
+@SuiteClasses({ /*PlayerTest.class*/ })
 public class DomainTestSuite {
 
 }
